9b6d295e958ab4b2a339448c662e32fa03db9b9f
KlimukVI <KlimukVI@rambler.ru>
2020-10-04 22:31:14 +0300

    feat: add problem solver

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let memory = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+       if (arr[i] === bracketsConfig[j][0]) {
+        if (bracketsConfig[j][0] === bracketsConfig[j][1] && arr[i] == memory[memory.length-1]) {
+          memory.pop();
+        } else {
+          memory.push(arr[i]);
+        }
+      }
+      else if (arr[i] === bracketsConfig[j][1]) {
+        if (memory.length === 0) { return false; }
+        if (bracketsConfig[j][0] === memory[memory.length-1]) {
+          memory.pop();
+        } else { return false;}
+      }
+     }
+  }
+  return memory.length === 0;
 }
